f2162b55c7db5bdfecb30488d91e85956843c3e5
HeathHuang <heathhuang0701@gmail.com>
2016-06-30 11:51:42 +0800

    [mod] 將lettuce連線與註冊頻道移到thread裡，繼續觀察

--- app/src/main/java/heath/android/sample/im/lettuce/MessagingService.java
+++ app/src/main/java/heath/android/sample/im/lettuce/MessagingService.java
@@ -7,11 +7,14 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.event.Event;
+import com.lambdaworks.redis.event.EventBus;
 import com.lambdaworks.redis.pubsub.RedisPubSubConnection;
 import com.lambdaworks.redis.pubsub.RedisPubSubListener;
 
 import heath.android.sample.Config;
 import heath.android.sample.utils.FileUtils;
+import rx.functions.Action1;
 
 /**
  * Created by heath on 2016/6/22.
@@ -31,64 +34,24 @@ public class MessagingService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(Config.TAG, "MessagingService onCreate");
-        FileUtils.logToSDCard(getBaseContext(), "lettuce.log", "MessagingService onCreate");
+        FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "MessagingService onCreate");
 
-        client = RedisClient.create("redis://ce28bb56-c1da-46df-9288-7327c4317aafc50b778a-551b-4cd0-bbc8-b7c3a8733e51f5632a50-3066-4e6e-8a2c-88dd3a264379@54.169.219.115:6379");
-        connection = client.connectPubSub();
-        connection.addListener(new RedisPubSubListener<String, String>() {
-            @Override
-            public void message(String channel, String message) {
-                Log.d(Config.TAG, "channel:" + channel);
-                Log.d(Config.TAG, "message:" + message);
-                FileUtils.logToSDCard(getBaseContext(), "lettuce.log", "channel:" + channel + ", message:" + message);
-            }
-
-            @Override
-            public void message(String pattern, String channel, String message) {
-                Log.d(Config.TAG, "pattern:" + pattern);
-                Log.d(Config.TAG, "channel:" + channel);
-                Log.d(Config.TAG, "message:" + message);
-            }
-
-            @Override
-            public void subscribed(String channel, long count) {
-                Log.d(Config.TAG, "subscribed:" + System.currentTimeMillis());
-                Log.d(Config.TAG, "channel:" + channel);
-                Log.d(Config.TAG, "count:" + count);
-                FileUtils.logToSDCard(getBaseContext(), "lettuce.log", "subscribed channel:" + channel);
-            }
-
-            @Override
-            public void psubscribed(String pattern, long count) {
-
-            }
-
-            @Override
-            public void unsubscribed(String channel, long count) {
-                Log.d(Config.TAG, "channel:" + channel);
-                Log.d(Config.TAG, "count:" + count);
-                FileUtils.logToSDCard(getBaseContext(), "lettuce.log", "unsubscribed channel:" + channel);
-            }
-
-            @Override
-            public void punsubscribed(String pattern, long count) {
-
-            }
-        });
-        connection.subscribe("heath", "huang");
+        new Thread(subscribe).start();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d(Config.TAG, "MessagingService onDestroy");
-        FileUtils.logToSDCard(getBaseContext(), "lettuce.log", "MessagingService onDestroy");
+        FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "MessagingService onDestroy");
 
         if (connection != null && connection.isOpen()) {
             connection.close();
+            FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "connection closed");
         }
         if (client != null) {
             client.shutdown();
+            FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "client shutdown");
         }
     }
 
@@ -96,13 +59,75 @@ public class MessagingService extends Service {
     public void onTaskRemoved(Intent rootIntent) {
         super.onTaskRemoved(rootIntent);
         Log.d(Config.TAG, "MessagingService onTaskRemoved");
-        FileUtils.logToSDCard(getBaseContext(), "lettuce.log", "MessagingService onTaskRemoved");
+        FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "MessagingService onTaskRemoved");
 
         if (connection != null && connection.isOpen()) {
             connection.close();
+            FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "connection closed");
         }
         if (client != null) {
             client.shutdown();
+            FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "client shutdown");
         }
     }
+
+    private Runnable subscribe = new Runnable() {
+        @Override
+        public void run() {
+            client = RedisClient.create("redis://ce28bb56-c1da-46df-9288-7327c4317aafc50b778a-551b-4cd0-bbc8-b7c3a8733e51f5632a50-3066-4e6e-8a2c-88dd3a264379@54.169.219.115:6379");
+            EventBus eventBus = client.getResources().eventBus();
+            eventBus.get().subscribe(new Action1<Event>() {
+                @Override
+                public void call(Event event) {
+                    Log.d(Config.TAG, "event:" + event);
+                    FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "event:" + event);
+                }
+            });
+
+            Log.d(Config.TAG, "client start subscribe");
+            FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "client start subscribe");
+            connection = client.connectPubSub();
+            connection.addListener(new RedisPubSubListener<String, String>() {
+                @Override
+                public void message(String channel, String message) {
+                    Log.d(Config.TAG, "channel:" + channel);
+                    Log.d(Config.TAG, "message:" + message);
+                    FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "channel:" + channel + ", message:" + message);
+                }
+
+                @Override
+                public void message(String pattern, String channel, String message) {
+                    Log.d(Config.TAG, "pattern:" + pattern);
+                    Log.d(Config.TAG, "channel:" + channel);
+                    Log.d(Config.TAG, "message:" + message);
+                }
+
+                @Override
+                public void subscribed(String channel, long count) {
+                    Log.d(Config.TAG, "subscribed:" + System.currentTimeMillis());
+                    Log.d(Config.TAG, "channel:" + channel);
+                    Log.d(Config.TAG, "count:" + count);
+                    FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "subscribed channel:" + channel);
+                }
+
+                @Override
+                public void psubscribed(String pattern, long count) {
+
+                }
+
+                @Override
+                public void unsubscribed(String channel, long count) {
+                    Log.d(Config.TAG, "channel:" + channel);
+                    Log.d(Config.TAG, "count:" + count);
+                    FileUtils.logToSDCard(getBaseContext(), "lettuce.txt", "unsubscribed channel:" + channel);
+                }
+
+                @Override
+                public void punsubscribed(String pattern, long count) {
+
+                }
+            });
+            connection.subscribe("heath", "huang");
+        }
+    };
 }
